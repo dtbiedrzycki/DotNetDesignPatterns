50800674a9de0d3be627838e91ea316b067e6932
David Thor Biedrzycki <dtbiedrzycki@gmail.com>
2015-07-27 13:46:42 -0500

    Added singleton class (AlphaRobotoSingleton)

---
+++ DesignPatterns.Creational/Builder/Implementations/AlphaRobotoSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using DesignPatterns;
+using DesignPatterns.Implementations;
+
+namespace BuilderPatterns.Builder.Implementations
+{
+	public class AlphaRobotoSingleton : Roboto
+	{
+		private static readonly Lazy<AlphaRobotoSingleton> _instance = new Lazy<AlphaRobotoSingleton>(
+			() =>
+			{
+				var powerSource = new UnlimitedPowerSource();
+				var roboto = new AlphaRobotoSingleton()
+				{
+					PowerSource =  powerSource,
+					Arms = new List<IArm>()
+					{
+						new SimpleArm(new SimpleMotor(powerSource)),
+						new SimpleArm(new SimpleMotor(powerSource))
+					},
+					MobilitySystem = new BipedalMobilitySystem(new SimpleMotor(powerSource))
+				};
+
+				return roboto;
+			});
+
+		public AlphaRobotoSingleton Instance()
+		{
+			return _instance.Value;
+		}
+	}
+}
---
+++ DesignPatterns.Models/Implementations/UnlimitedPowerSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DesignPatterns.Implementations
+{
+	public class UnlimitedPowerSource : IPowerSource
+	{
+		public bool ProvidePower(int watts)
+		{
+			return true;
+		}
+
+		public bool Charge(int watts)
+		{
+			return true;
+		}
+
+		public int CurrentCharge
+		{
+			get { return Int32.MaxValue; }
+		}
+
+		public int Capacity
+		{
+			get { return Int32.MaxValue; }
+		}
+	}
+}
