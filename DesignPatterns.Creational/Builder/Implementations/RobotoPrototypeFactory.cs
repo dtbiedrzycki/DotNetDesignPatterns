56c8b4c31928306f91a9a25112406e3e69113f24
David Thor Biedrzycki <dtbiedrzycki@gmail.com>
2015-07-28 13:02:34 -0500

    Added RobotoPrototypeFactory

---
+++ DesignPatterns.Creational/Builder/Implementations/RobotoPrototypeFactory.cs
@@ -0,0 +1,26 @@
+﻿using DesignPatterns.Implementations;
+
+namespace BuilderPatterns.Builder.Implementations
+{
+	public class RobotoPrototypeFactory : IRobotoFactory
+	{
+		private readonly Roboto _rollingRoboto;
+		private readonly Roboto _humanoidRoboto;
+
+		public RobotoPrototypeFactory(Roboto rollingRoboto, Roboto humanoidRoboto)
+		{
+			_rollingRoboto = rollingRoboto;
+			_humanoidRoboto = humanoidRoboto;
+		}
+
+		public Roboto CreateRollingRoboto()
+		{
+			return _rollingRoboto.Clone() as Roboto;
+		}
+
+		public Roboto CreateHumanoidRoboto()
+		{
+			return _humanoidRoboto.Clone() as Roboto;
+		}
+	}
+}
