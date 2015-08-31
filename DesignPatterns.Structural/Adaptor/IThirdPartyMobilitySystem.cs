c511ca4b686882344c5eceaf2ee980dde5f6b6a3
David Thor Biedrzycki <dtbiedrzycki@gmail.com>
2015-08-31 15:25:18 -0500

    Added Adaptor class

---
+++ DesignPatterns.Structural/Adaptor/IThirdPartyMobilitySystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DesignPatterns.Structural.Adaptor
+{
+	public interface IThirdPartyMobilitySystem
+	{
+		/// <summary>
+		/// (x,y,z) coordinates tuple
+		/// </summary>
+		Tuple<int, int, int> Coordinates { get; }
+
+		/// <summary>
+		/// Stops any and all movements
+		/// </summary>
+		void Stop();
+
+		/// <summary>
+		/// Starts forward movement
+		/// </summary>
+		void BeginMoveForward();
+
+		/// <summary>
+		/// Starts backwards movement
+		/// </summary>
+		void BeginMoveBackward();
+
+		/// <summary>
+		/// Starts movement towards the left
+		/// </summary>
+		void BeginMoveLeft();
+		
+		/// <summary>
+		/// Starts movement towards the right
+		/// </summary>
+		void BeginMoveRight();
+	}
+}
---
+++ DesignPatterns.Structural/Adaptor/ThirdPartyArmAdaptor.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading;
+
+namespace DesignPatterns.Structural.Adaptor
+{
+	public class ThirdPartyArmAdaptor : IMobilitySystem
+	{
+		private readonly IThirdPartyMobilitySystem _thirdPartyMobilitySystem;
+		
+		public ThirdPartyArmAdaptor(IThirdPartyMobilitySystem thirdPartyMobilitySystem)
+		{
+			_thirdPartyMobilitySystem = thirdPartyMobilitySystem;
+		}
+
+		public Coordinates CurrentCoordinates
+		{
+			get
+			{
+				var coordinates = new Coordinates()
+				{
+					X = _thirdPartyMobilitySystem.Coordinates.Item1,
+					Y = _thirdPartyMobilitySystem.Coordinates.Item2,
+					Z = _thirdPartyMobilitySystem.Coordinates.Item3
+				};
+
+				return coordinates;
+			}
+		}
+
+		public void MoveForward()
+		{
+			int targetY = _thirdPartyMobilitySystem.Coordinates.Item2 + 1;
+
+			_thirdPartyMobilitySystem.BeginMoveForward();
+
+			while (_thirdPartyMobilitySystem.Coordinates.Item2 < targetY)
+			{
+				Thread.Sleep(1);
+			}
+			
+			_thirdPartyMobilitySystem.Stop();
+		}
+
+		public void MoveBackward()
+		{
+			int targetY = _thirdPartyMobilitySystem.Coordinates.Item2 - 1;
+
+			_thirdPartyMobilitySystem.BeginMoveBackward();
+
+			while (_thirdPartyMobilitySystem.Coordinates.Item2 > targetY)
+			{
+				Thread.Sleep(1);
+			}
+
+			_thirdPartyMobilitySystem.Stop();
+		}
+
+		public void MoveLeft()
+		{
+			int targetX = _thirdPartyMobilitySystem.Coordinates.Item1 - 1;
+
+			_thirdPartyMobilitySystem.BeginMoveLeft();
+
+			while (_thirdPartyMobilitySystem.Coordinates.Item2 > targetX)
+			{
+				Thread.Sleep(1);
+			}
+
+			_thirdPartyMobilitySystem.Stop();
+		}
+
+		public void MoveRight()
+		{
+			int targetX = _thirdPartyMobilitySystem.Coordinates.Item1 + 1;
+
+			_thirdPartyMobilitySystem.BeginMoveLeft();
+
+			while (_thirdPartyMobilitySystem.Coordinates.Item2 < targetX)
+			{
+				Thread.Sleep(1);
+			}
+
+			_thirdPartyMobilitySystem.Stop();
+		}
+
+		public object Clone()
+		{
+			return MemberwiseClone();
+		}
+	}
+}
