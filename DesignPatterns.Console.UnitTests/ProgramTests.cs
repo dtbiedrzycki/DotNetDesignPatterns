eda84af8cdab3dcd74fb99c95e506a61d33e4cd6
dtbiedrzycki <dtbiedrzycki@gmail.com>
2015-11-12 12:18:25 -0600

    Added unit tests for the Program.Excute using the Factory, Builder, and Prototype methods

--- DesignPatterns.Console.UnitTests/ProgramTests.cs
+++ DesignPatterns.Console.UnitTests/ProgramTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
@@ -21,6 +19,9 @@ namespace DesignPatterns.Console.UnitTests
 		private Mock<IWriter> _writer;
 		private Mock<IReader> _reader;
 		private Mock<ISingletonGetRobotoMethod> _singletonGetRobotoMethod;
+		private Mock<IFactoryGetRobotoMethod> _factoryGetRobotoMethod;
+		private Mock<IBuilderGetRobotMethod> _builderGetRobotoMethod;
+		private Mock<IPrototypeGetRobotoMethod> _prototypeGetRobotoMethod;
 		private Mock<IRobotoProgram> _robotoProgram;
 
 		[TestInitialize]
@@ -33,6 +34,9 @@ namespace DesignPatterns.Console.UnitTests
 			_writer = _mockRepository.Create<IWriter>(MockBehavior.Loose);
 			_reader = _mockRepository.Create<IReader>();
 			_singletonGetRobotoMethod = _mockRepository.Create<ISingletonGetRobotoMethod>();
+			_factoryGetRobotoMethod = _mockRepository.Create<IFactoryGetRobotoMethod>();
+			_builderGetRobotoMethod = _mockRepository.Create<IBuilderGetRobotMethod>();
+			_prototypeGetRobotoMethod = _mockRepository.Create<IPrototypeGetRobotoMethod>();
 			_robotoProgram = _mockRepository.Create<IRobotoProgram>();
 
 			// Set up stubs
@@ -43,6 +47,9 @@ namespace DesignPatterns.Console.UnitTests
 			_windsorContainer.Register(Component.For<IWriter>().Instance(_writer.Object));
 			_windsorContainer.Register(Component.For<IReader>().Instance(_reader.Object));
 			_windsorContainer.Register(Component.For<ISingletonGetRobotoMethod>().Instance(_singletonGetRobotoMethod.Object));
+			_windsorContainer.Register(Component.For<IFactoryGetRobotoMethod>().Instance(_factoryGetRobotoMethod.Object));
+			_windsorContainer.Register(Component.For<IBuilderGetRobotMethod>().Instance(_builderGetRobotoMethod.Object));
+			_windsorContainer.Register(Component.For<IPrototypeGetRobotoMethod>().Instance(_prototypeGetRobotoMethod.Object));
 			_windsorContainer.Register(Component.For<IRobotoProgram>().Instance(_robotoProgram.Object));
 		}
 
@@ -51,8 +58,8 @@ namespace DesignPatterns.Console.UnitTests
 		{
 			// Arrange
 			Roboto roboto = new Roboto();
-			_singletonGetRobotoMethod.Setup(x => x.GetRoboto()).Returns(roboto);
 			_reader.Setup(x => x.ReadLine()).Returns("1");
+			_singletonGetRobotoMethod.Setup(x => x.GetRoboto()).Returns(roboto);
 			_robotoProgram.Setup(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)));
 
 			// Act
@@ -61,6 +68,66 @@ namespace DesignPatterns.Console.UnitTests
 			// Assert
 			_reader.Verify(x => x.ReadLine(), Times.Once);
 			_singletonGetRobotoMethod.Verify(x => x.GetRoboto(), Times.Once);
+			_robotoProgram.Verify(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)), Times.Once);
+
+			_mockRepository.VerifyAll();
+		}
+
+		[TestMethod]
+		public void Execute_FactoryGetRobotoMethod_GoldFlow()
+		{
+			// Arrange
+			Roboto roboto = new Roboto();
+			_reader.Setup(x => x.ReadLine()).Returns("2");
+			_factoryGetRobotoMethod.Setup(x => x.GetRoboto()).Returns(roboto);
+			_robotoProgram.Setup(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)));
+
+			// Act
+			Program.Execute(_windsorContainer);
+
+			// Assert
+			_reader.Verify(x => x.ReadLine(), Times.Once);
+			_factoryGetRobotoMethod.Verify(x => x.GetRoboto(), Times.Once);
+			_robotoProgram.Verify(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)), Times.Once);
+
+			_mockRepository.VerifyAll();
+		}
+
+		[TestMethod]
+		public void Execute_BuilderGetRobotoMethod_GoldFlow()
+		{
+			// Arrange
+			Roboto roboto = new Roboto();
+			_reader.Setup(x => x.ReadLine()).Returns("3");
+			_builderGetRobotoMethod.Setup(x => x.GetRoboto()).Returns(roboto);
+			_robotoProgram.Setup(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)));
+
+			// Act
+			Program.Execute(_windsorContainer);
+
+			// Assert
+			_reader.Verify(x => x.ReadLine(), Times.Once);
+			_builderGetRobotoMethod.Verify(x => x.GetRoboto(), Times.Once);
+			_robotoProgram.Verify(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)), Times.Once);
+
+			_mockRepository.VerifyAll();
+		}
+
+		[TestMethod]
+		public void Execute_PrototypeGetRobotoMethod_GoldFlow()
+		{
+			// Arrange
+			Roboto roboto = new Roboto();
+			_reader.Setup(x => x.ReadLine()).Returns("4");
+			_prototypeGetRobotoMethod.Setup(x => x.GetRoboto()).Returns(roboto);
+			_robotoProgram.Setup(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)));
+
+			// Act
+			Program.Execute(_windsorContainer);
+
+			// Assert
+			_reader.Verify(x => x.ReadLine(), Times.Once);
+			_prototypeGetRobotoMethod.Verify(x => x.GetRoboto(), Times.Once);
 			_robotoProgram.Verify(x => x.Execute(It.Is<IEnumerable<Roboto>>(y => y.Count() == 1 && y.First() == roboto)), Times.Once);
 
 			_mockRepository.VerifyAll();
